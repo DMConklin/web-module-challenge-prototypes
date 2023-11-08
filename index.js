3c14a81ad831203864329d42461721d8bdadfb69
DMConklin <dmconklin84@gmail.com>
2023-11-08 00:33:24 -0600

    Completed Tasks 1-4

--- index.js
+++ index.js
@@ -15,8 +15,21 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
+  Person.prototype.eat = function(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood)
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach = []
+  }
+  Person.prototype.toString = function() {
+    return `${name}, ${age}`
+  }
 }
 
 
@@ -36,8 +49,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+  Car.prototype.fill = function(gallons) {
+    this.tank+= gallons
+  }
 }
 
 
@@ -49,18 +68,23 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
+  Baby.prototype.play = function() {
+    return `Playing with ${favoriteToy}`
+  }
 }
 
+Baby.prototype = Object.create(Person.prototype)
 
 /* 
   TASK 4
-  In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  In your own words explain the four principles for the 'this' keyword below:
+  1. Window/Global Object Binding: The default global scope binding
+  2. Implicit Binding: When 'this' refers to the object calling it
+  3. New Binding: When 'this' refers to a newly created instance of an object using the 'new' keyword
+  4. Explicit Binding: When we purposefully bind 'this' to an object using a method ex .call
 */
 
 ///////// END OF CHALLENGE /////////
